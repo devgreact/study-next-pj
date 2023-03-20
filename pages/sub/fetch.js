0766704c20aea6089e0d328cd1d9b5c47feb130e
devgreact <dev.greact@gmail.com>
2023-03-20 23:49:40 +0900

    출력

--- pages/sub/fetch.js
+++ pages/sub/fetch.js
@@ -1,16 +1,19 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const Fetch = () => {
+    const [user, setUser] = useState({name:null})
     useEffect(() => {
         fetch(process.env.NEXT_PUBLIC_API_URL+'api/hello')
         .then(res => res.json())
         .then(result=> {
             console.log(result)
+            setUser(result);
         })
     }, [])
     return (
       <>
           <h1>현재 페이지 : /pages/sub/fetch.js</h1>
+          <p>{user.name}</p>
           링크(/) : <a href="/">/pages/index.js</a>
       </>
     )
